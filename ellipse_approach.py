3ae736aea5aaa1d2d41eaeb1ce771de94f90584b
valeria-alex <47971956+valeria-alex@users.noreply.github.com>
2021-02-05 19:03:57 +0100

    Update ellipse_approach.py

--- ellipse_approach.py
+++ ellipse_approach.py
@@ -1,5 +1,5 @@
 
-#Copyright (C) 2020  Valeria Alexandra Feraru
+# Copyright (C) 2020  Valeria Alexandra Feraru
 
 
 # This program is free software; you can redistribute it and/or
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Contact: valeria.feraru97@gmail.com
+
 #!usr/bin/env python 
 
 
